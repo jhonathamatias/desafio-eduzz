a801051bd6a09638db434e549fc58e3a3d2ee5e1
Jhonatha Matias <jhonathamatias35@gmail.com>
2025-10-04 22:06:42 -0300

    feat: add cors config

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,10 +1,12 @@
+import cors from 'cors';
 import express from 'express';
 
-import routes from '../routes';
+import routes from '@/routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 export default app;
